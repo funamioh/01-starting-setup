2caf49cdc2b764984b033f3f271921ca22ec4b97
Miho F <85437491+funamioh@users.noreply.github.com>
2021-12-15 22:46:35 +0900

    Add properties tojs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 21),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        ddate={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        ddate={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        ddate={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        ddate={expenses[0].date}
+      ></ExpenseItem>
     </div>
   );
 }
