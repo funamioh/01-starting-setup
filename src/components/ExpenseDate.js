52315194ba726dd7247101e5141c6935ae717fb8
Miho F <85437491+funamioh@users.noreply.github.com>
2021-12-21 23:56:12 +0900

    Add a new component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -3,7 +3,7 @@ import "./ExpenseDate.css";
 function ExpenseDate(props) {
   console.log(props);
   const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.getDate("en-US", { day: "2-digit" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
